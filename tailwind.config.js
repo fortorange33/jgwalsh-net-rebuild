61139782d9699d2f646b39103465e66270f5dfed
JG Walsh <104898878+fortorange33@users.noreply.github.com>
2025-05-10 23:26:59 +0000

    Update navbar and content structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './index.html',
+    './about.html',
+    './projects.html',
+    './contact.html',
+    './experience.html',
+    './skills.html',
+    './terms-of-service.html',
+    './privacy-policy.html',
+    './thanks.html',
+    './card.html',
+    './404.html',
+    './partials/header.html',
+    './partials/footer.html',
+    './assets/js/**/*.js'
+  ],
   theme: {
     extend: {},
   },
