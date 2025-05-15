87b47272d49a8b5e548bcf2412c6e721cfe86640
JG Walsh <wombats.simmers-0e@icloud.com>
2025-05-15 08:32:05 -0400

    Refactor Tailwind CSS configuration for improved content paths and update header navigation links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './about.html',
-    './projects.html',
-    './contact.html',
-    './experience.html',
-    './skills.html',
-    './terms-of-service.html',
-    './privacy-policy.html',
-    './thanks.html',
-    './card.html',
-    './404.html',
-    './partials/header.html',
-    './partials/footer.html',
-    './assets/js/**/*.js'
-  ],
+  content: ["./**/*.html", "./partials/**/*.html"],
   theme: {
     extend: {},
   },
